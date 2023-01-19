746619a29125fa8b29f102fd722e1a6032e3a284
Khess S Dotto <khessdotto@gmail.com>
2023-01-19 17:58:21 -0300

    checkpoint2 - 18h - 19.01.2023

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ console.log('runninggg');
 const playBtn = document.getElementById("playBtn");
 
 window.addEventListener('load', () => {
+    const gameSection = document.getElementById('game-section');
+    gameSection.style.display = "none";
+
     playBtn.addEventListener('click', () => {
         const preGameSection = document.getElementById('pre-game-section');
-        const game-section = document.getElementById('game-section');
+
+        preGameSection.style.display = "none";
+        gameSection.style.display = "flex";
+    
+        const backToMenuBtn = document.getElementById('back-to-menu-btn');
+
+        backToMenuBtn.addEventListener('click', () => {
+            gameSection.style.display = "none";
+            preGameSection.style.display = "block";
+        })
     })
 })
 
