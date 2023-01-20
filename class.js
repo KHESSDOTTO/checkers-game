497afaf581a80ad644f99cfed36a91ae73170aa8
Khess S Dotto <khessdotto@gmail.com>
2023-01-20 18:36:01 -0300

    createBoard and selectedPiece added (ok). working on the other ones.

--- class.js
+++ class.js
@@ -1,44 +1,61 @@
-class checkers {
-    constructor {
+class Checkers {
+    constructor () {
         this.board = [
             ['white', 'b', 'white', 'b', 'white', 'b', 'white', 'b'],
             ['b', 'white', 'b', 'white', 'b', 'white', 'b', 'white'],
             ['white', 'b', 'white', 'b', 'white', 'b', 'white', 'b'],
             ['black', 'white', 'black', 'white', 'black', 'white', 'black', 'white'],
             ['white', 'black', 'white', 'black', 'white', 'black', 'white', 'black'],
-            ['white', 'w', 'white', 'w', 'white', 'w', 'white', 'w'],
             ['w', 'white', 'w', 'white', 'w', 'white', 'w', 'white'],
             ['white', 'w', 'white', 'w', 'white', 'w', 'white', 'w'],
-        ]
+            ['w', 'white', 'w', 'white', 'w', 'white', 'w', 'white'],
+        ];
         this.selectedPiece = undefined;
     }
 
-    createBoard() {
+    createBoard () {
         // cria o tabuleiro e posiciona as peças para que o jogo possa começar. Chama white turn. 
+        const boardHTML = document.getElementById('board').querySelector('table');
+        boardHTML.innerHTML = '';
         this.board.forEach((outerElement, outerIndex) => {
-            const boardHTML = document.getElementById('board').querySelector('.board');
             const newRow = document.createElement("tr");
-            boardHTML.appendChild
-                
-            element.forEach((innerElement, innerIndex) => {
-                const newColumn = document.createElement("td")
+            boardHTML.appendChild(newRow);
+
+            outerElement.forEach((innerElement, innerIndex) => {
+                const newColumn = document.createElement("td");
+                newRow.appendChild(newColumn);
+                newColumn.setAttribute('id', `${outerIndex}${innerIndex}`);
+
+                if (this.board[outerIndex][innerIndex] === 'white') {
+                    newColumn.classList.add('white-square');
+                }else if (this.board[outerIndex][innerIndex] === 'b') {
+                    newColumn.innerHTML = 'b'; // substituir por imagem de peça marrom.
+                    newColumn.classList.add('black-square');
+                }else if (this.board[outerIndex][innerIndex] === 'w') {
+                    newColumn.innerHTML = 'w'; // substituir por imagem de peça branca.
+                    newColumn.classList.add('black-square');
+                }else {
+                    newColumn.classList.add('black-square');
+                };
             });
         });
-    }
+    };
 
-    selectPiece() {
+    selectPiece(selectedPiece) {
         // altera o valor da "this.selectedPiece" para a peça selecionada.
+        this.selectedPiece = selectedPiece;
     }
 
     whiteTurn() {
         // habilita eventListeners nos botões brancos e desabilita dos marrons.
+
     }
 
     brownTurn() {
         // habilita eventListeners nos botões marrons e desabilita dos brancos.
     }
 
-    move(selected, direction) {
+    move(direction) {
         // movimenta as peças do tabuleiro conforme for solicitado. Chama diversas funções "checks".
         // Avisa caso nenhuma peça tenha sido selecionada quando um botão de movimento for clicado. se for uma dama, chama a
         // função moveKing. passa a vez do jogador após a movimentação ser concluída.
@@ -72,7 +89,22 @@ class checkers {
 
     winVerify() {
         // verifica o término do jogo após as jogadas whiteTurn e brownTurn. Caso tenha encerrado o jogo. Anuncia na tela
-    }
+        let browns = 0;
+        let whites = 0;
+        const squares = document.getElementById('game-section').querySelector('table').children;
+        squares.forEach(element => {
+            if (element.innerHTML === 'b') {
+                browns = 1;
+            }else if (element.innerHTML === 'w') {
+                whites = 1;
+            }
+        });
+        if (browns === 0) {
+            alert(`White player wins!`);
+        }else if (white === 0) {
+            alert(`Brown player wins!`);
+        };
+    };
 
     formKing() {
         // forma uma dama quando uma peça chega ao extremo vertical oposto do tabuleiro.
@@ -99,5 +131,5 @@ class checkers {
     chooseComboKing() {
         // chooseCombo considerando a movimentação diferenciada da dama.
     }
-}
+};
 
--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-console.log('runninggg');
-
 const playBtn = document.getElementById("playBtn");
 
 window.addEventListener('load', () => {
-    const gameSection = document.getElementById('game-section');
-    gameSection.style.display = "none";
-
     playBtn.addEventListener('click', () => {
+        let game = new Checkers();
+        game.createBoard();
         const preGameSection = document.getElementById('pre-game-section');
-
+        const gameSection = document.getElementById('game-section');
         preGameSection.style.display = "none";
         gameSection.style.display = "flex";
-    
         const backToMenuBtn = document.getElementById('back-to-menu-btn');
-
         backToMenuBtn.addEventListener('click', () => {
             gameSection.style.display = "none";
             preGameSection.style.display = "block";
         })
+        game.whiteTurn();
+        game.winVerify();
     })
 })
 
